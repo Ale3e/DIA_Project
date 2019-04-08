eb663b802864a5b6aa3eecca1596104fc5b0936e
Luca_Bianco <luca3.bianco@mail.polimi.it>
2019-04-08 12:29:20 +0200

    a_b cycle and plot

--- src/a_b_n_test.py
+++ src/a_b_n_test.py
@@ -5,52 +5,49 @@ from algorithms.a_b_n.data import *
 
 
 p_A = 0.06
-p_B = 0.032
-p_C = 0.015
 
+p = np.array([0.05, 0.04, 0.03, 0.02, 0.01, 0.005, 0.001])
 # A is control; B is test
 N_A = 1000
 N_B = 1000
 N_C = 1000
-
-
-ab_data = generate_data(N_A, N_B, p_A, p_B, days=365)
-ab_data1 = generate_data(N_A, N_B, p_A, p_C, days=365)
-
-ab_summary = ab_data.pivot_table(values='converted', index='group', aggfunc=np.sum)
-# add additional columns to the pivot table
-ab_summary1 = ab_data1.pivot_table(values='converted', index='group', aggfunc=np.sum)
-
-ab_summary['total'] = ab_data.pivot_table(values='converted', index='group', aggfunc=lambda x: len(x))
-ab_summary['rate'] = ab_data.pivot_table(values='converted', index='group')
-ab_summary1['total'] = ab_data1.pivot_table(values='converted', index='group', aggfunc=lambda x: len(x))
-ab_summary1['rate'] = ab_data1.pivot_table(values='converted', index='group')
-
-
-A_converted=ab_summary['converted'][0]
-A_total=ab_summary['total'][0]
-A_cr=ab_summary['rate'][0]
-B_converted=ab_summary['converted'][1]
-B_total=ab_summary['total'][1]
-B_cr=ab_summary['rate'][1]
-C_converted=ab_summary1['converted'][1]
-C_total=ab_summary1['total'][1]
-C_cr=ab_summary1['rate'][1]
+ab_data=[]
+ab_summary=[]
+for n in range(len(p)):
+    ab_data.append(generate_data(N_A, N_B, p_A, p[n], days=365))
+    ab_summary.append(ab_data[n].pivot_table(values='converted', index='group', aggfunc=np.sum))
+    ab_summary[n]['total'] = ab_data[n].pivot_table(values='converted', index='group', aggfunc=lambda x: len(x))
+    ab_summary[n]['rate'] = ab_data[n].pivot_table(values='converted', index='group')
 
 
 
+A_converted=ab_summary[0]['converted'][0]
+A_total=ab_summary[0]['total'][0]
+A_cr=ab_summary[0]['rate'][0]
+
+total=[]
+converted=[]
+cr=[]
+
+total.append(ab_summary[0]['total'][0])
+cr.append(ab_summary[0]['converted'][0])
+converted.append(ab_summary[0]['rate'][0])
 fig, ax = plt.subplots(figsize=(12,6))
 xA = np.linspace(A_converted-49, A_converted+50, 100)
 yA = scs.binom(A_total, A_cr).pmf(xA)
 ax.bar(xA, yA, alpha=0.5)
-xB = np.linspace(B_converted-49, B_converted+50, 100)
-yB = scs.binom(B_total, B_cr).pmf(xB)
-ax.bar(xB, yB, alpha=0.5)
-xC = np.linspace(C_converted-49, C_converted+50, 100)
-yC = scs.binom(C_total, C_cr).pmf(xB)
-ax.bar(xC, yC, alpha=0.5)
-
-ax.legend(['Group A', 'Group B', 'Group C'])
+ax.plot(xA, yA, alpha=0.5)
+x=[]
+y=[]
+for n in range(len(p)):
+    total.append(ab_summary[n]['total'][1])
+    converted.append(ab_summary[n]['converted'][1])
+    cr.append(ab_summary[n]['rate'][1])
+    x.append(np.linspace(converted[n] - 49, converted[n] + 50, 100))
+    y.append(scs.binom(total[n], cr[n]).pmf(x[n]))
+    ax.bar(x[n], y[n], alpha=0.5)
+    ax.plot(x[n], y[n], alpha=0.5)
+ax.legend(['Group A', 'Group B', 'Group C', 'Group D', 'Group E', 'Group F', 'Group G', 'Group H'])
 
 
 
@@ -68,5 +65,3 @@ plt.show()
 # ax.bar(xB, yB, alpha=0.5)
 # plt.xlabel('converted')
 # plt.ylabel('probability')
-
-print(ab_summary['rate'][0] - ab_summary['rate'][1])
