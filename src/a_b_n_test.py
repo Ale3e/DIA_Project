4eadda8e3cb7e32611c3b36ccceeb829b98f02a6
Luca_Bianco <luca3.bianco@mail.polimi.it>
2019-04-08 10:49:54 +0200

    Changes on ab

--- src/a_b_n_test.py
+++ src/a_b_n_test.py
@@ -5,15 +5,15 @@ from algorithms.a_b_n.data import *
 
 
 # code examples presented in Python
-bcr = 0.10  # baseline conversion rate
-d_hat = 0.02  # difference between the groups
+p_A = 0.04  # baseline conversion rate
+p_B = 0.032  # difference between the groups
 
 # A is control; B is test
 N_A = 1000
 N_B = 1000
 
 
-ab_data = generate_data(N_A, N_B, bcr, d_hat)
+ab_data = generate_data(N_A, N_B, p_A, p_B)
 
 ab_summary = ab_data.pivot_table(values='converted', index='group', aggfunc=np.sum)
 # add additional columns to the pivot table
@@ -30,8 +30,6 @@ B_converted=ab_summary['converted'][1]
 B_total=ab_summary['total'][1]
 B_cr=ab_summary['rate'][1]
 
-p_A=0.10
-p_B=0.12
 
 
 fig, ax = plt.subplots(figsize=(12,6))
@@ -42,15 +40,15 @@ ax.axvline(x=B_cr * A_total, c='blue', alpha=0.75, linestyle='--')
 plt.xlabel('converted')
 plt.ylabel('probability')
 
-fig, ax = plt.subplots(figsize=(12,6))
-xA = np.linspace(A_converted-49, A_converted+50, 100)
-yA = scs.binom(A_total, p_A).pmf(xA)
-ax.bar(xA, yA, alpha=0.5)
-
-xB = np.linspace(B_converted-49, B_converted+50, 100)
-yB = scs.binom(B_total, p_B).pmf(xB)
-ax.bar(xB, yB, alpha=0.5)
-plt.xlabel('converted')
-plt.ylabel('probability')
+# fig, ax = plt.subplots(figsize=(12,6))
+# xA = np.linspace(A_converted-49, A_converted+50, 100)
+# yA = scs.binom(A_total, p_A).pmf(xA)
+# ax.bar(xA, yA, alpha=0.5)
+#
+# xB = np.linspace(B_converted-49, B_converted+50, 100)
+# yB = scs.binom(B_total, p_B).pmf(xB)
+# ax.bar(xB, yB, alpha=0.5)
+# plt.xlabel('converted')
+# plt.ylabel('probability')
 
 plt.show()
