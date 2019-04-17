c0b3d852e5bacd332df5224724b739c8fadbecf7
Luca_Bianco <luca3.bianco@mail.polimi.it>
2019-04-17 16:45:44 +0200

    some changes ab

--- src/a_b_n_test.py
+++ src/a_b_n_test.py
@@ -4,39 +4,46 @@ import pandas as ps
 from algorithms.a_b_n.data import *
 
 
-p_A = 0.06
 
-p = np.array([0.05, 0.04, 0.03, 0.02, 0.01, 0.005, 0.001])
+
+p = np.array([0.0263, 0.0193, 0.0129, 0.0061, 0.0012])
 # A is control; B is test
-N_A = 1000
-N_B = 1000
-N_C = 1000
-ab_data=[]
-ab_summary=[]
+price = list(range(325, 450, 25))
+N_A = 10000
+N_B = 10000
+N_C = 10000
+ab_data = []
+best = np.random.randint(0, 5)
+ab_summary = []
 for n in range(len(p)):
-    ab_data.append(generate_data(N_A, N_B, p_A, p[n], days=365))
+    print(best, n)
+    if best == n:
+        continue
+    ab_data.append(generate_data(N_A, N_B, p[best], p[n], days=365))
     ab_summary.append(ab_data[n].pivot_table(values='converted', index='group', aggfunc=np.sum))
     ab_summary[n]['total'] = ab_data[n].pivot_table(values='converted', index='group', aggfunc=lambda x: len(x))
     ab_summary[n]['rate'] = ab_data[n].pivot_table(values='converted', index='group')
+    if ab_summary[n]['converted'][0] * price[best] < ab_summary[n]['converted'][1] * price[n]:
+        best = n
 
 
-
-A_converted=ab_summary[0]['converted'][0]
-A_total=ab_summary[0]['total'][0]
-A_cr=ab_summary[0]['rate'][0]
+# A_converted=ab_summary[0]['converted'][1]
+# A_total=ab_summary[0]['total'][1]
+# A_cr=ab_summary[0]['rate'][1]
 
 total=[]
 converted=[]
 cr=[]
 
-total.append(ab_summary[0]['total'][0])
-cr.append(ab_summary[0]['converted'][0])
-converted.append(ab_summary[0]['rate'][0])
+# total.append(ab_summary[0]['total'][0])
+# cr.append(ab_summary[0]['converted'][0])
+# converted.append(ab_summary[0]['rate'][0])
+#
+# xA = np.linspace(A_converted-49, A_converted+50, 100)
+# yA = scs.binom(A_total, A_cr).pmf(xA)
 fig, ax = plt.subplots(figsize=(12,6))
-xA = np.linspace(A_converted-49, A_converted+50, 100)
-yA = scs.binom(A_total, A_cr).pmf(xA)
-ax.bar(xA, yA, alpha=0.5)
-ax.plot(xA, yA, alpha=0.5)
+# ax.bar(xA, yA, alpha=0.5)
+# ax.plot(xA, yA, alpha=0.5)
 x=[]
 y=[]
 for n in range(len(p)):
@@ -45,9 +52,12 @@ for n in range(len(p)):
     cr.append(ab_summary[n]['rate'][1])
     x.append(np.linspace(converted[n] - 49, converted[n] + 50, 100))
     y.append(scs.binom(total[n], cr[n]).pmf(x[n]))
-    ax.bar(x[n], y[n], alpha=0.5)
+    if n == best:
+        ax.bar(x[n], y[n], alpha=0.5)
     ax.plot(x[n], y[n], alpha=0.5)
-ax.legend(['Group A', 'Group B', 'Group C', 'Group D', 'Group E', 'Group F', 'Group G', 'Group H'])
+array_legend = ['Price 325', 'Price 350', 'Price 375', 'Price 400', 'Price 425']
+array_legend[best] += "-Best"
+ax.legend(array_legend)
 
 
 
