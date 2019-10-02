91dd68b7bbeade69778d497912e04f28335dda2c
Alessandro Carughi <10455590@polimi.it>
2019-10-02 11:17:54 +0200

    CG

--- src/algorithms/ContextGen.py
+++ src/algorithms/ContextGen.py
@@ -1,42 +1,36 @@
 import numpy as np
 
-#   users: array tipo
-#
-#   [[Worker, Student, Retired],
-#    [Male, Female, Unknown]]
-#
-#
-#   disagg: array tipo
-#
-#   [[0.75, 0.2, 0.05],
-#    [0.25, 0.30, 0.45]]
-#
-#
-#   prob: array tipo
-    [
-    #first split
-    [
-    #worker
+# users: array tipo
+
+# [[Worker, Student, Retired],
+# [Male, Female, Unknown]]
+
+
+# disagg: array tipo
+
+{[0.75, 0.2, 0.05],
+ [0.25, 0.30, 0.45]}
+
+# prob: array tipo
+# first split
+{[
+    # worker
     [0.0312, 0.0224, 0.014, 0.0068, 0.0016],
-    #student
+    # student
     [0.0245, 0.0157, 0.0112, 0.005, 0.0007],
-    #retired
+    # retired
     [0.0312, 0.0224, 0.014, 0.0068, 0.0016]
-    ],
-    #second split
+],
+    # second split
     [
-    # male
-    [0.0312, 0.0224, 0.014, 0.0068, 0.0016],
-    # female
-    [0.0312, 0.0224, 0.014, 0.0068, 0.0016],
-    # unknown
-    [0.0312, 0.0224, 0.014, 0.0068, 0.0016]
-    ]
-    ]
-#
-#
-#
-#
+        # male
+        [0.0312, 0.0224, 0.014, 0.0068, 0.0016],
+        # female
+        [0.0312, 0.0224, 0.014, 0.0068, 0.0016],
+        # unknown
+        [0.0312, 0.0224, 0.014, 0.0068, 0.0016]
+    ]}
+
 
 class ContextGen:
     def __init__(self, users):
