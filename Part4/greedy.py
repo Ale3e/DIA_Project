745f31c6060537715b48179d9cb17bbc310725e5
miclunetti <miclunetti@gmail.com>
2020-06-05 11:56:00 +0200

    MC greedy uncomplete 2

--- Part4/greedy.py
+++ Part4/greedy.py
@@ -25,58 +25,62 @@ if __name__ == "__main__":
 
     features = [0.10, 0.08, 0.05, 0.02]
 
-    graph = generate_graph(10000, 5, 0.05, 123)
+    graph = generate_graph(100, 5, 0.05, 123)
     graph = weight_edges(graph, features)
     graph = weight_nodes(graph)
 
-    budget = 100
+    budget = 10
 
     start_time = time.time()
     seeds = []
     remaining_budget = budget
     epsilon = 0.1
-    delta = 0.2
+    delta = 0.9
 
     # evaluate each node in the graph for marginal increase in greedy algorithm
     marginal_gain = dict.fromkeys(graph.nodes, 0)
     nodes_left_to_evaluate = set(marginal_gain.keys())
-    set_len = len(nodes_left_to_evaluate)
-    print(set_len)
-    print(type(set_len))
 
+    print(nodes_left_to_evaluate)
+    print(type(nodes_left_to_evaluate))
+    print(graph.nodes(data='cost'))
+    print(type((graph.nodes(data='cost'))))
+    print(graph.nodes.data('cost'))
+    var = min(graph.nodes(data='cost'))
+    print(var)
 
-    #while remaining_budget >=0 and remaining_budget >= min(graph.nodes(data='cost')):
+    while remaining_budget >= 0 and remaining_budget >= min(graph.nodes(data='cost')) and nodes_left_to_evaluate:
 
-    for node in tqdm.tqdm(range(set_len)):
+        for n in nodes_left_to_evaluate:
 
-        cost = graph.nodes[node]['cost']
-        n_simulations = int((1 / (epsilon ** 2)) * np.log(len(seeds + [node]) + 1) * np.log(1 / delta))
-        IC_cumulative = 0.0
+            cost = graph.nodes[n]['cost']
+            n_simulations = int((1 / (epsilon ** 2)) * np.log(len(seeds + [n]) + 1) * np.log(1 / delta))
+            IC_cumulative = 0.0
 
-        for n in range(n_simulations):
-            node_list = []
-            IC_result = information_cascade(graph, node)[0]
-            IC_cumulative += IC_result
+            for simulation in range(n_simulations):
+                node_list = []
+                IC_result = information_cascade(graph, n)[0]
+                IC_cumulative += IC_result
 
-        spread = round(((IC_cumulative/n_simulations) / cost), 5)
-        marginal_gain[node] = spread
+            spread = round(((IC_cumulative / n_simulations) / cost), 3)
+            marginal_gain[n] = spread
 
-    #prendi best nodo da aggiungere ai seeds (max(marginal_gain)
-    #togliere nodo aggiunto ai seed dalla lista @node_left_to_evaluate
-    #ripeti
+        spread_max = max(marginal_gain.values())
+        index_max = list(marginal_gain.keys())[list(marginal_gain.values()).index(spread_max)]
+        remaining_budget -= graph.nodes[index_max]['cost']
+        seeds.append(index_max)
 
+        nodes_left_to_evaluate.remove(index_max)
+        marginal_gain.pop(index_max)
 
+    # prendi best nodo da aggiungere ai seeds (max(marginal_gain)
+    # togliere nodo aggiunto ai seed dalla lista @node_left_to_evaluate
+    # ripeti
 
-    #print(min(graph.nodes(data='cost')))
+    # print(min(graph.nodes(data='cost')))
 
-    #while remaining_budget >=0 and remaining_budget >= min(graph.nodes(data='cost')):
+    # while remaining_budget >=0 and remaining_budget >= min(graph.nodes(data='cost')):
 
+    print(seeds)
     print(marginal_gain)
-    print(round(sum(marginal_gain.values())/len(marginal_gain), 5))
-
-
-
-
-
-
-
+    print(round(sum(marginal_gain.values()) / len(marginal_gain), 3))
