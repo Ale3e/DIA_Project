58ef8d58709549602a0f77989efc1a7dc124b871
miclunetti <miclunetti@gmail.com>
2020-07-18 14:57:31 +0200

    TS UCB1 e greedy

--- Part1/Pricing_UCB1_learner.py
+++ Part1/Pricing_UCB1_learner.py
@@ -10,7 +10,7 @@ class UCB1Learner:
 
         self.n_arms = n_arms
         self.t = 0
-        self.rewards_per_arm = x = [[] for i in range(n_arms)]
+        self.rewards_per_arm = [0.0 for i in range(n_arms)]
         self.collected_rewards = np.array([])
         self.marginal_profit = marginal_profit
         self.empirical_means_no_bound = copy.deepcopy(marginal_profit)
@@ -29,13 +29,12 @@ class UCB1Learner:
     def update(self, pulled_arm, reward):
 
         self.t += 1
-        self.rewards_per_arm[pulled_arm].append(reward*self.marginal_profit[pulled_arm])
-        self.collected_rewards = np.append(self.collected_rewards, reward*self.marginal_profit[pulled_arm])
+        self.rewards_per_arm[pulled_arm] += (reward*self.marginal_profit[pulled_arm])
+        self.collected_rewards += reward*self.marginal_profit[pulled_arm]
         if self.sliding_window == 0:
             self.n_of_samples[pulled_arm] += 1
-        for i in range(self.n_arms):
-            self.empirical_means_no_bound[i] = np.sum(self.rewards_per_arm[i]) / self.n_of_samples[i]
-            self.empirical_means[i] = self.empirical_means_no_bound[i] + np.sqrt((2 * np.log(self.t+1)) / self.n_of_samples[i])
+            self.empirical_means_no_bound[pulled_arm] = self.rewards_per_arm[pulled_arm] / self.n_of_samples[pulled_arm]
+            self.empirical_means[pulled_arm] = self.empirical_means_no_bound[pulled_arm] + np.sqrt((2 * np.log(self.t+1)) / self.n_of_samples[pulled_arm])
 
 
 
@@ -44,24 +43,60 @@ class UCB1Learner:
 if __name__ == '__main__':
 
     n_arms = 4
-    n_experiments = 100
+    n_experiments = 1000
     T = 365
 
-    p = np.array([0.363, 0.3, 0.229, 0.11])
+    p = np.array([0.363, 0.3, 0.23, 0.12])
     marginal_profit = [2.5, 5.0, 7.5, 10]
+    opt_idx = 2
 
     ucb_learner = UCB1Learner(n_arms, marginal_profit)
-    print(ucb_learner.marginal_profit)
-
     env = PricingEnvironment(n_arms, p)
+    daily_rewards_UCB1 = []
+    daily_rewards_TS = []
 
     for time in range(T):
 
-        for customer in range(n_experiments):
+        rewards_UCB1 = []
+        rewards_TS = []
 
-            pulled_arm = ucb_learner.pull_arm()
-            reward = env.round(pulled_arm)
-            print('Pulled arm: {} with price {}, reward is {}'.format(pulled_arm, ucb_learner.marginal_profit[pulled_arm], reward))
-            ucb_learner.update(pulled_arm, reward)
-    print('REWARD: {}'.format(np.sum(ucb_learner.collected_rewards)))
+        for customer in range(n_experiments):
+            pulled_arm_UCB1 = ucb_learner.pull_arm()
+            reward_UCB1 = env.round(pulled_arm_UCB1)
+            ucb_learner.update(pulled_arm_UCB1, reward_UCB1)
+            profit_UCB1 = reward_UCB1 * marginal_profit[pulled_arm_UCB1]
+            rewards_UCB1.append(profit_UCB1)
+
+
+        print('Day {}, reward UCB1: {} '.format(time, np.sum(rewards_UCB1)))
+        daily_rewards_UCB1.append(np.sum(rewards_UCB1))
+
+    print('Yearly profit with UCB1: {}'.format(np.sum(daily_rewards_UCB1)))
+
+    ### PLOT ###
+
+    plt.style.use('seaborn')  # pretty matplotlib plots
+    plt.rcParams['figure.figsize'] = (12, 8)
+    opt = []
+    for t in range(T): opt.append((marginal_profit[opt_idx]*p[opt_idx])*n_experiments)
+
+
+    plt.plot((daily_rewards_UCB1), color='blue', label='UCB1')
+    plt.plot((opt), color='red', label='opt')
+    plt.xlabel('t')
+    plt.ylabel('Profit')
+    plt.title('Profit comparison')
+    plt.legend()
+    plt.show()
+
+    regret_UCB1 = [x1 - x2 for (x1, x2) in zip(opt, daily_rewards_UCB1)]
+
+    plt.plot(np.cumsum(regret_UCB1), color='blue', label='UCB1')
+    plt.xlabel('t')
+    plt.ylabel('Regret')
+    plt.title('Regret Comparison')
+    plt.legend()
+    plt.show()
+
+
 
--- Part1/TS_UCB1_comparison.py
+++ Part1/TS_UCB1_comparison.py
@@ -1,29 +1,36 @@
 from Part1.Pricing_TS_learner import *
 from Part1.Pricing_UCB1_learner import *
+from Part1.Pricing_greedy_learner import *
 
 if __name__ == '__main__':
 
     n_arms = 4
     n_experiments = 1000
+    mc_simulation = 10
     T = 365
 
     p = np.array([0.363, 0.3, 0.23, 0.12])
     marginal_profit = [2.5, 5.0, 7.5, 10]
-    opt_idx = 3
+    opt_idx = 2
 
     ucb_learner = UCB1Learner(n_arms, marginal_profit)
     ts_learner = TSLearner(n_arms, marginal_profit)
+    g_learner = GreedyLearner(n_arms, marginal_profit)
 
     env = PricingEnvironment(n_arms, p)
     daily_rewards_UCB1 = []
     daily_rewards_TS = []
+    daily_rewards_g = []
+
 
     for time in range(T):
 
         rewards_UCB1 = []
         rewards_TS = []
+        rewards_g = []
 
         for customer in range(n_experiments):
+
             pulled_arm_UCB1 = ucb_learner.pull_arm()
             reward_UCB1 = env.round(pulled_arm_UCB1)
             ucb_learner.update(pulled_arm_UCB1, reward_UCB1)
@@ -36,38 +43,48 @@ if __name__ == '__main__':
             profit_TS = reward_TS * marginal_profit[pulled_arm_TS]
             rewards_TS.append(profit_TS)
 
-        print('Day {}, reward UCB1: {}, reward TS: {} '.format(time, np.sum(profit_UCB1), np.sum(profit_TS)))
-        daily_rewards_UCB1.append(np.sum(reward_UCB1))
-        daily_rewards_TS.append(np.sum(reward_TS))
+            pulled_arm_g = g_learner.pull_arm()
+            reward_g = env.round(pulled_arm_g)
+            g_learner.update(pulled_arm_g, reward_g)
+            profit_g = reward_g * marginal_profit[pulled_arm_g]
+            rewards_g.append(profit_g)
+
+
+        print('Day {}, reward UCB1: {}, reward TS: {} '.format(time, np.sum(rewards_UCB1), np.sum(rewards_TS)))
+        daily_rewards_UCB1.append(np.sum(rewards_UCB1))
+        daily_rewards_TS.append(np.sum(rewards_TS))
+        daily_rewards_g.append(np.sum(rewards_g))
 
     print('Yearly profit with UCB1: {}'.format(np.sum(daily_rewards_UCB1)))
-    print('Yearly profit with UCB1: {}'.format(np.sum(daily_rewards_TS)))
-
-
+    print('Yearly profit with TS: {}'.format(np.sum(daily_rewards_TS)))
+    print('Yearly profit with greedy: {}'.format(np.sum(daily_rewards_g)))
 
     ### PLOT ###
 
     plt.style.use('seaborn')  # pretty matplotlib plots
     plt.rcParams['figure.figsize'] = (12, 8)
     opt = []
-    for t in range(T): opt.append(marginal_profit[opt_idx]*p[opt_idx])
+    for t in range(T): opt.append((marginal_profit[opt_idx]*p[opt_idx])*n_experiments)
 
-    plt.plot(np.cumsum(daily_rewards_UCB1), color='blue', label='UCB1')
-    plt.plot(np.cumsum(daily_rewards_TS), color='green', label='TS')
-    # plt.plot(spreads_LUCB, color='orange', label='LinUCB')
-    plt.plot(opt, color='red', label='opt')
+
+    plt.plot((daily_rewards_UCB1), color='blue', label='UCB1')
+    plt.plot((daily_rewards_TS), color='green', label='TS')
+    plt.plot(daily_rewards_g, color='orange', label='greedy')
+    plt.plot((opt), color='red', label='opt')
     plt.xlabel('t')
     plt.ylabel('Profit')
     plt.title('Profit comparison')
     plt.legend()
     plt.show()
 
-    regret_UCB1 = np.abs(opt - daily_rewards_UCB1)
-    regret_TS = np.abs(opt - daily_rewards_TS)
+    regret_UCB1 = [np.abs(x1 - x2) for (x1, x2) in zip(opt, daily_rewards_UCB1)]
+    regret_TS = [np.abs(x1 - x2) for (x1, x2) in zip(opt, daily_rewards_TS)]
+    regret_g = [np.abs(x1 - x2) for (x1, x2) in zip(opt, daily_rewards_g)]
+
 
     plt.plot(np.cumsum(regret_UCB1), color='blue', label='UCB1')
     plt.plot(np.cumsum(regret_TS), color='green', label='TS')
-    # plt.plot(np.cumsum(regret_LUCB), color='orange', label='LinUCB')
+    plt.plot(np.cumsum(regret_g), color='orange', label='greedy')
     plt.xlabel('t')
     plt.ylabel('Regret')
     plt.title('Regret Comparison')
